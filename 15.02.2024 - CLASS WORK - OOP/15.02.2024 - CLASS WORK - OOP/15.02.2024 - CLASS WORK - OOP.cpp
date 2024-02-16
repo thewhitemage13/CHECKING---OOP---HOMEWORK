8356955e75a05139b690cfeccc6c19cbac154cfd
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2024-02-16 17:35:38 +0100

    Add files via upload

---
+++ 15.02.2024 - CLASS WORK - OOP/15.02.2024 - CLASS WORK - OOP/15.02.2024 - CLASS WORK - OOP.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+void Checking(string math)
+{
+	stack<char> parentheses;
+
+	parentheses.push(math[0]);
+
+	for (int i = 1; i < math.length(); i++)
+	{
+		if (math[i] == '(' || math[i] == '[' || math[i] == '{')
+		{
+			parentheses.push(math[i]);
+		}
+		else if (math[i] == ')')
+		{
+			if (parentheses.top() == '(')
+				parentheses.pop();
+			else
+				break;
+		}
+		else if (math[i] == ']')
+		{
+			if (parentheses.top() == '[')
+				parentheses.pop();
+			else
+				break;
+		}
+		else if (math[i] == '}')
+		{
+			if (parentheses.top() == '{')
+				parentheses.pop();
+			else
+				break;
+		}
+	}
+
+	if (parentheses.empty())
+	{
+		cout << "correct sequence" << "\n";
+
+	}
+	else
+	{
+		cout << "mismatch" << "\n";
+	}
+}
+
+int main()
+{
+	string math = "{(1<5->[3+]9-4)}";
+	Checking(math);
+}
